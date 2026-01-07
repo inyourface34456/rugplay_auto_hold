8dc253321dc5335c2c710bd60bdf045c10e6702a
inyourface3445 <trenten46@gmail.com>
2026-01-07 02:09:44 -0500

    renamed files

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import time
 import os
 
 cookies = {
@@ -7,53 +6,60 @@ cookies = {
     '__Secure-better-auth.session_token': os.getenv("SECURE_BETTER_AUTH"),
 }
 
-def get_portfolio_data():
-    headers = {
-        'User-Agent': os.getenv("USER_AGENT"),
-        'Accept': '*/*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Referer': "https://rugplay.com",
-        'Connection': 'keep-alive',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'Priority': 'u=4',
-    }
+headers = {
+    'User-Agent': os.getenv("USER_AGENT"),
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://rugplay.com/coin/HOLDCONST',
+    'Content-Type': 'application/json',
+    'Origin': f'https://rugplay.com/coin/HOLDCONST',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'Priority': 'u=0',
+}
 
-    return requests.get('https://rugplay.com/api/portfolio/total', cookies=cookies, headers=headers).json()
-
-def sell_coin(symbol: str, amt: int):
-    headers = {
-        'User-Agent': os.getenv("USER_AGENT"),
-        'Accept': '*/*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Referer': 'https://rugplay.com/coin/6N2M2ZIXML',
-        'Content-Type': 'application/json',
-        'Origin': f'https://rugplay.com/coin/{symbol}',
-        'Connection': 'keep-alive',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'Priority': 'u=0',
-    }
+def sell_coin(amt: float):
+    data = requests.get("https://rugplay.com/api/coin/HOLDCONST/", cookies=cookies, headers=headers).json()
 
     json_data = {
         'type': 'SELL',
-        'amount': amt,
+        'amount': float(amt/float(data["coin"]["currentPrice"])),
     }
 
-    requests.post(f'https://rugplay.com/api/coin/{symbol}/trade', cookies=cookies, headers=headers, json=json_data)
+    print(json_data)
+    data = requests.post(f'https://rugplay.com/api/coin/HOLDCONST/trade', cookies=cookies, headers=headers, json=json_data)
+    print(data.json())
 
+def buy_coin(amt: float):
+    json_data = {
+        'type': 'BUY',
+        'amount': float(amt),
+    }
+
+    print(json_data)
+    data = requests.post(f'https://rugplay.com/api/coin/HOLDCONST/trade', cookies=cookies, headers=headers, json=json_data)
+    print(data.json())
+
+def get_sell_amt():
+    data = requests.get("https://rugplay.com/api/coin/HOLDCONST/", cookies=cookies, headers=headers).json()
+    current = float(data["coin"]["poolBaseCurrencyAmount"])
+
+    return 5000000-current
 
 if __name__ == '__main__':
-    response = get_portfolio_data()
-    coin_names = []
-    for i in response["coinHoldings"]:
-        # print(i["symbol"])
-        if i["symbol"] == "IDEGAF" or i["symbol"] == "IDEGAF":
-            print("skipped idegaf")
+    while True:
+        amt = get_sell_amt()
+        try:
+            if amt == 0:
+                continue
+            elif amt < 0:
+                sell_coin(amt.__abs__())
+            else:
+                buy_coin(amt.__abs__())
+        except Exception as e:
+            print(f"[ERROR] {e}")
             continue
-        sell_coin(i["symbol"], i["quantity"])
-        print(f"sold ${i['symbol']} {i['quantity']}")
-        time.sleep(.05)
 
+        print(f"off by {amt}")
